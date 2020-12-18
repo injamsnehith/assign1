af422abe0a12848de031eb1b3041ed22e62c0f46
INJAM SNEHITH <injam.snehith@terralogic.com>
2020-12-18 11:49:44 +0530

    assignment

---
+++ src/menubar.jsx
@@ -0,0 +1,25 @@
+  
+import React, { Component } from 'react';
+import img1 from './images/image.jpg';
+
+
+class menubar extends Component {
+    render() {
+        return (
+            <div>
+            <h1 className="menubar">MenuBar</h1>
+            
+            <div className="img">
+            <img src={img1} alt=""/><br />
+            <img src='./images/image.jpg' alt=""/><br />
+            <a href="/#page1"> <button className="btn btn-primary" style={{width:100 + '%'}}>Page1</button><hr /></a>            
+            <a href="/#page2"> <button className="btn btn-primary" style={{width:100 + '%'}}>Page2</button><hr /></a>
+            </div>
+</div>  
+               
+          
+        );
+    }
+}
+
+export default menubar;
